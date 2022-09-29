e745cbffc6426cdc0591aeffa1f815e8203d85f7
Nicholas Roberts <niccolox@devekko.com>
2022-09-29 14:19:49 -0700

    Phoenix components

---
+++ lib/phoenix_components_daisy.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixComponentsDaisy do
+  @moduledoc """
+  Documentation for `PhoenixComponentsDaisy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixComponentsDaisy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_components_daisy_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixComponentsDaisyTest do
+  use ExUnit.Case
+  doctest PhoenixComponentsDaisy
+
+  test "greets the world" do
+    assert PhoenixComponentsDaisy.hello() == :world
+  end
+end
