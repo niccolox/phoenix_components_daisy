f449738ac8c79cb5bb95f90c14fc1851a3d00b0a
Nicholas Roberts <niccolo.roberts@gmail.com>
2022-09-30 18:09:07 -0700

    phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -47,15 +47,15 @@ defmodule PhoenixComponentsDaisy.MixProject do
       # {:ex_doc, "~> 0.28.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:phoenix, github: "devekkoio/phoenix", override: true}
+      # {:phoenix, github: "devekkoio/phoenix", override: true}
       # {:phoenix, "~> 1.6"},
       #       {:phoenix_ecto, "~> 4.4"},
       #       {:ecto_sql, "~> 3.6"},
       #       {:postgrex, ">= 0.0.0"},
       #       {:phoenix_html, "~> 3.0"},
       #       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      #       {:phoenix_live_view, "~> 0.18"},
-      # #      {:phoenix_components_daisy, "lib/phoenix_components_daisy"},      
+             {:phoenix_live_view, "~> 0.18"},
+      #       {:phoenix_components_daisy, "lib/phoenix_components_daisy"},      
       #       {:heroicons, "~> 0.5"},
       #       {:floki, ">= 0.30.0", only: :test},
       #       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
