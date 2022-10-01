e51157f38588e0f21affccc5805aff72ba01532e
Nicholas Roberts <niccolo.roberts@gmail.com>
2022-09-30 18:53:03 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -18,7 +18,6 @@ defmodule PhoenixComponentsDaisy.MixProject do
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
